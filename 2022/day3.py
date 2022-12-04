0ed9527929e21d9f71667041aa73fa5508c97a4b
Niraj Palecha <8666468+niraj8@users.noreply.github.com>
2022-12-05 00:57:26 +0530

    2022: day3 part2

--- 2022/day3.py
+++ 2022/day3.py
@@ -19,10 +19,19 @@ def part1(rucksacks):
         compartment2 = set(rucksack[int(len(rucksack)/2):])
         priorities_sum += priority[compartment1.intersection(compartment2).pop()]
     return priorities_sum
-    
+
+
+def part2(rucksacks):
+    priorities_sum = 0
+    for i in range(0,int(len(rucksacks)/3)):
+        rucksack_group = rucksacks[i*3 : i*3+3]
+        sack_set_list = [set(sack) for sack in rucksack_group]
+        badge = sack_set_list[0].intersection(sack_set_list[1]).intersection(sack_set_list[2])
+        priorities_sum += priority[badge.pop()]
+    return priorities_sum
 
 
 with open("day3.in", "r") as reader:
     rucksacks = parse(reader.read())
     print(part1(rucksacks))
-
+    print(part2(rucksacks))
