5b89357cf7fc931cba818b46be9c1268207422da
Niraj Palecha <8666468+niraj8@users.noreply.github.com>
2022-12-06 21:55:23 +0530

    day4: part1

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,17 +1,31 @@
-
 def parse(buffer):
     def str_range_to_int_range(str_range):
         return [int(x) for x in str_range.split("-")]
 
-    return [ 
-        [ str_range_to_int_range(ranges[0]), str_range_to_int_range(ranges[1]) ] for ranges in [line.split(",") for line in buffer.split("\n")]
+    return [
+        [str_range_to_int_range(ranges[0]), str_range_to_int_range(ranges[1])]
+        for ranges in [line.split(",") for line in buffer.split("\n")]
     ]
 
+
 def has_full_overlap(pair):
-    return True
+    range1 = pair[0]
+    range2 = pair[1]
+
+    # list starting with the smaller number or the larger list should be range1
+    if range1[0] == range2[0]:
+        if range1[1] < range2[1]:
+            range1, range2 = range2, range1
+    elif range1[0] > range2[0]:
+        range1, range2 = range2, range1
+
+    return range1[1] >= range2[1]
 
 
-with open("day4-example.in", "r") as reader:
+def part1(pairs):
+    return len(list(filter(has_full_overlap, pairs)))
+
+
+with open("day4.in", "r") as reader:
     pairs = parse(reader.read())
-    print(len(list(filter(has_full_overlap, pairs))))
-    
+    print(part1(pairs))
