bf10f01469d80e78c9743c9512658661728310b6
Niraj Palecha <8666468+niraj8@users.noreply.github.com>
2022-12-06 22:03:45 +0530

    day4: part2

--- 2022/day4.py
+++ 2022/day4.py
@@ -8,27 +8,33 @@ def parse(buffer):
     ]
 
 
-def has_full_overlap(pair):
-    range1 = pair[0]
-    range2 = pair[1]
-
+def reorder_range_list(range1, range2):
     # list starting with the smaller number or the larger list should be range1
     if range1[0] == range2[0]:
         if range1[1] < range2[1]:
             range1, range2 = range2, range1
     elif range1[0] > range2[0]:
         range1, range2 = range2, range1
+    return range1, range2
 
+
+def has_full_overlap(pair):
+    range1, range2 = reorder_range_list(pair[0], pair[1])
     return range1[1] >= range2[1]
 
+
 def has_overlap(pair):
-    return True
+    range1, range2 = reorder_range_list(pair[0], pair[1])
+    return range2[0] <= range1[1] or range1[1] > range2[1]
+
 
 def part1(pairs):
     return len(list(filter(has_full_overlap, pairs)))
 
+
 def part2(pairs):
     return len(list(filter(has_overlap, pairs)))
+
 
 with open("day4.in", "r") as reader:
     pairs = parse(reader.read())
