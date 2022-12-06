d814e79d52eb95d3570fc20b0b1bc5963ccfdad4
Niraj Palecha <8666468+niraj8@users.noreply.github.com>
2022-12-06 22:11:53 +0530

    day4: rewrite with range()

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,6 +1,7 @@
 def parse(buffer):
     def str_range_to_int_range(str_range):
-        return [int(x) for x in str_range.split("-")]
+        int_list = [int(x) for x in str_range.split("-")]
+        return range(int_list[0], int_list[1])
 
     return [
         [str_range_to_int_range(ranges[0]), str_range_to_int_range(ranges[1])]
@@ -10,22 +11,22 @@ def parse(buffer):
 
 def reorder_range_list(range1, range2):
     # list starting with the smaller number or the larger list should be range1
-    if range1[0] == range2[0]:
-        if range1[1] < range2[1]:
+    if range1.start == range2.start:
+        if range1.stop < range2.stop:
             range1, range2 = range2, range1
-    elif range1[0] > range2[0]:
+    elif range1.start > range2.start:
         range1, range2 = range2, range1
     return range1, range2
 
 
 def has_full_overlap(pair):
     range1, range2 = reorder_range_list(pair[0], pair[1])
-    return range1[1] >= range2[1]
+    return range1.stop >= range2.stop
 
 
 def has_overlap(pair):
     range1, range2 = reorder_range_list(pair[0], pair[1])
-    return range2[0] <= range1[1] or range1[1] > range2[1]
+    return range2.start <= range1.stop or range1.stop > range2.stop
 
 
 def part1(pairs):
