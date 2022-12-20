dc361003d069a81fae94f8e458cda1cbc91a26c2
Niraj Palecha <8666468+niraj8@users.noreply.github.com>
2022-12-20 22:55:23 +0530

    day8: part 2

--- 2022/day8.py
+++ 2022/day8.py
@@ -45,11 +45,56 @@ def visible(x_coord, y_coord, tree_heights_matrix):
            visible_from_bottom(x_coord, y_coord, tree_heights_matrix)
 
 
-with open("day8.in", "r") as reader:
-    tree_heights = parse_input(reader.read())
+def part1():
     visible_trees_count = 4 * len(tree_heights) - 4
     for y in range(1, len(tree_heights) - 1):
         for x in range(1, len(tree_heights) - 1):
             if visible(x, y, tree_heights):
                 visible_trees_count += 1
-    print(visible_trees_count)
+    return visible_trees_count
+
+
+def scenic_score(x_coord, y_coord, tree_heights_matrix, transposed):
+    tree_height = tree_heights_matrix[x_coord][y_coord]
+
+    trees_on_left = list(reversed(tree_heights_matrix[x_coord][0:y_coord]))
+    trees_on_right = tree_heights_matrix[x_coord][y_coord + 1:]
+    trees_on_top = list(reversed(transposed[y_coord][:x_coord]))
+    trees_on_bottom = transposed[y_coord][x_coord + 1:]
+
+    visible_left = trees_visible(tree_height, trees_on_left)
+    visible_top = trees_visible(tree_height, trees_on_top)
+    visible_right = trees_visible(tree_height, trees_on_right)
+    visible_bottom = trees_visible(tree_height, trees_on_bottom)
+
+    return visible_left * visible_top * visible_right * visible_bottom
+
+
+def trees_visible(this_tree_height, heights):
+    count = 0
+    for height in heights:
+        if height < this_tree_height:
+            count += 1
+        elif height >= this_tree_height:
+            count += 1
+            break
+    return count
+
+
+def part2(tree_heights_matrix):
+    max_score = 0
+    transposed = list(map(list, zip(*tree_heights_matrix)))
+
+    for q in range(0, len(tree_heights)):
+        for p in range(0, len(tree_heights)):
+            tree_score = scenic_score(p, q, tree_heights_matrix, transposed)
+            if tree_score > max_score:
+                max_score = tree_score
+    return max_score
+
+
+with open("day8.in", "r") as reader:
+    tree_heights = parse_input(reader.read())
+
+    print(part1())
+    print(part2(tree_heights))
